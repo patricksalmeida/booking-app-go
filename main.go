bbb4ed0aca3a617fe80e47da83f5342bab1d1f66
Patrick Almeida <patricksalmeida@gmail.com>
2023-03-18 21:56:56 -0300

    using Printf instead of Println and use explicit types

--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	var conferenceName string = "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
 
-	fmt.Println("Welcome to our", conferenceName, "booking application")
-	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+
+	fmt.Printf("Welcome to our %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
+	var username string = "Tom"
+	var userTickets int = 2
+	fmt.Printf("User %v booked %v tickets\n", username, userTickets)
 }
